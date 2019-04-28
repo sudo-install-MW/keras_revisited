c3538bfa1dc69c7f45e56c449db2ab47697b60cc
mash-compute <maheshwaran.umapathy@my.utsa.edu>
2019-04-28 18:29:04 -0500

    training model added

---
+++ model.py
@@ -0,0 +1,30 @@
+from keras import layers
+from keras import models
+
+
+class Model:
+    def create_model(self):
+
+        model = models.Sequential()
+
+        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+        model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPool2D((2, 2)))
+
+        model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+        model.add(layers.MaxPool2D((2, 2)))
+
+        model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+        model.add(layers.MaxPool2D((2, 2)))
+
+        model.add(layers.Flatten())
+        model.add(layers.Dense(512, activation='relu'))
+
+        model.add(layers.Dense(1, activation='sigmoid'))
+
+        return model
+
+
+
---
+++ trainer.py
@@ -0,0 +1,53 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras_revisited.model import Model
+from keras import optimizers
+import matplotlib.pyplot as plt
+
+train_dir = "/media/mash-compute/eatsmart/DeepLearning/keras_revisited/data/data_processed/train"
+val_dir = "/media/mash-compute/eatsmart/DeepLearning/keras_revisited/data/data_processed/validation"
+
+# preprocess image
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(150, 150),
+                                                    batch_size=20,
+                                                    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(val_dir,
+                                                        target_size=(150, 150),
+                                                        batch_size=20,
+                                                        class_mode='binary')
+
+classifier = Model()
+model = classifier.create_model()
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+model.save('cats_and_dogs_small_1.h5')
+
+# plotting accuracy and training graph
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
