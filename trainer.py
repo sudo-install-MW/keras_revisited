0d9fa06afd66d0eaa60a590c973926c156dd9e65
mash-compute <maheshwaran.umapathy@my.utsa.edu>
2019-04-29 20:04:00 -0500

    training model script with data augmentation

--- trainer.py
+++ trainer.py
@@ -7,17 +7,24 @@ train_dir = "/media/mash-compute/eatsmart/DeepLearning/keras_revisited/data/data
 val_dir = "/media/mash-compute/eatsmart/DeepLearning/keras_revisited/data/data_processed/validation"
 
 # preprocess image
-train_datagen = ImageDataGenerator(rescale=1. / 255)
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(train_dir,
                                                     target_size=(150, 150),
-                                                    batch_size=20,
+                                                    batch_size=32,
                                                     class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(val_dir,
                                                         target_size=(150, 150),
-                                                        batch_size=20,
+                                                        batch_size=32,
                                                         class_mode='binary')
 
 classifier = Model()
@@ -28,7 +35,7 @@ model.compile(loss='binary_crossentropy',
 
 history = model.fit_generator(train_generator,
                               steps_per_epoch=100,
-                              epochs=30,
+                              epochs=100,
                               validation_data=validation_generator,
                               validation_steps=50)
 
