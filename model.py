9d537934b28d64b2e570eb2e1422f89ae0927593
mash-compute <maheshwaran.umapathy@my.utsa.edu>
2019-04-29 20:55:11 -0500

    trainable script ready

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 from keras import layers
 from keras.applications import VGG16
+from keras import models
 
 
 class Model:
     def create_model(self):
-
-        # model = models.Sequential()
+        model = models.Sequential()
         #
         # model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
         # model.add(layers.MaxPooling2D((2, 2)))
@@ -24,11 +24,14 @@ class Model:
         # model.add(layers.Dense(512, activation='relu'))
         #
         # model.add(layers.Dense(1, activation='sigmoid'))
-        model = VGG16(weights='imagenet',
-                      include_top=False,
-                      input_shape=(150, 150, 3))
+        model_vgg_base = VGG16(weights='imagenet',
+                               include_top=False,
+                               input_shape=(150, 150, 3),)
 
+        model.add(model_vgg_base)
+
+        model.add(layers.Flatten())
+        model.add(layers.Dense(256, activation='relu'))
+        model.add(layers.Dense(1, activation='sigmoid'))
+        model_vgg_base.trainable = False
         return model
-
-
-
--- trainer.py
+++ trainer.py
@@ -13,29 +13,33 @@ train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    height_shift_range=0.2,
                                    shear_range=0.2,
                                    zoom_range=0.2,
-                                   horizontal_flip=True)
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(train_dir,
                                                     target_size=(150, 150),
-                                                    batch_size=32,
+                                                    batch_size=128,
                                                     class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(val_dir,
                                                         target_size=(150, 150),
-                                                        batch_size=32,
+                                                        batch_size=128,
                                                         class_mode='binary')
 
 classifier = Model()
 model = classifier.create_model()
+
+print(model.trainable_weights)
+
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.RMSprop(lr=1e-4),
+              optimizer=optimizers.RMSprop(lr=2e-5),
               metrics=['acc'])
 
 history = model.fit_generator(train_generator,
                               steps_per_epoch=100,
-                              epochs=100,
+                              epochs=30,
                               validation_data=validation_generator,
                               validation_steps=50)
 
